d030ef88216ece60e985400f4eeded3d9fb188c1
Kranthi Juttiga <kranthij58@gmail.com>
2024-08-25 23:15:48 +0530

    made some changes made the checkout order summary interactive

--- data/cart.js
+++ data/cart.js
@@ -3,12 +3,12 @@ if(!cart) {
   cart = [{
   productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6" ,
   quantity: 2,
-  deliveryOptionsId : 1,
+  deliveryOptionId : '1',
 },
 {
   productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d" ,
   quantity: 1,
-  deliveryOptionsId : 2 ,
+  deliveryOptionId : '2' ,
 }];
 }
 
@@ -29,7 +29,7 @@ export function addToCart(productId,selectedQuantity){
       cart.push({
         productId: productId,
         quantity: selectedQuantity,
-        deliveryOptionsId : 1,
+        deliveryOptionId : '1',
       });
     }
    saveLocalStorage();
@@ -46,3 +46,16 @@ export function addToCart(productId,selectedQuantity){
   cart = modifiedCart;
   saveLocalStorage();
  }
+ export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveLocalStorage();
+}
--- scripts/homepageitems.js
+++ scripts/homepageitems.js
@@ -1,5 +1,6 @@
 import {products} from '../data/products.js';
 import {cart , addToCart} from '../data/cart.js';
+import formatCurrency from './utils/money.js';
 let productsGridHtml = "";
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.cart-quantity').innerHTML = `${updateQunatity()}`;
@@ -24,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${product.priceCents / 100}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
