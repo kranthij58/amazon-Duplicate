2944e0533cc3938dd5c242b1aef24e10fa69a4ba
Kranthi Juttiga <kranthij58@gmail.com>
2024-09-15 17:14:30 +0530

    manually testes the format currency function

---
+++ tests/moneytest.js
@@ -0,0 +1,14 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+console.log(formatCurrency(232356));
+console.log('test suite : format currency');
+console.log('test case 1: integer value');
+if (formatCurrency(2323)=== '23.23'){
+  console.log('test case passed');
+}
+if ( formatCurrency(0) === '0.00' ){
+  console.log('test case passed');
+
+}
+if (formatCurrency(2324.2333) === '23.24'){
+  console.log('test case passed');
+}
